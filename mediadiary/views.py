00b19b14eae5f6c447c6ad7f12b8656f69ab52c6
cmdrspartacus <tobiasorlando@gmail.com>
2016-05-30 12:44:09 +0800

    added view to handle submitted form
    submitted data rendered in entrycomplete.html
    
    [ticket: 4]

--- mediadiary/views.py
+++ mediadiary/views.py
@@ -1,17 +1,24 @@
 from mediadiary import app
-from flask import render_template, redirect, url_for
+from flask import render_template
 from .forms import MediaItemEntry
+from flask import request
 
 
 @app.route('/')
 def index():
     return render_template('landing.html', title='Chronoviz')
 
+
 @app.route('/entry', methods=['GET', 'POST'])
 def entry():
-    form = MediaItemEntry()
-    if form.is_submitted():
+    form = MediaItemEntry(request.form)
+    if request.method == 'POST' and form.is_submitted():
         # fields are assembled and submitted to the database.
+        entrycomplete = [form.name.data, form.media.data, form.review.data]
+        return render_template('entrycomplete.html', title='Entry Complete', entrycomplete=entrycomplete)
+    return render_template('entry.html', form=form)
 
-        return redirect(url_for('index'))
-    return render_template('entry.html', form= form)
+#
+# @app.route('/entrycomplete', methods=['POST'])
+# def completeEntry():
+#     return render_template('entrycomplete.html', title='Entry Complete', entrycomplete=entrycomplete)
